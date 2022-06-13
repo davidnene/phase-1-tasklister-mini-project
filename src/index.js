a2ed824f1bc637d605cd093067f1b47e100e99a5
davidnene <davienesh4@gmail.com>
2022-06-13 21:25:46 +0300

    Update code in index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+ let form = document.querySelector('form')
+ form.addEventListener('submit', (e) => {
+   e.preventDefault() //prevents auto-refresh
+   handleToDo(e.target.new.value)
+   form.reset() //removes input display after submit
+ })
 });
+
+//function to handle incoming input
+function handleToDo(todo) {
+  let tasks = document.createElement('li') //creates new 'ul'
+  document.querySelector('#tasks').appendChild(tasks)
+  tasks.textContent = `${todo} `
+  let btn = document.createElement('button') //creates a button
+  btn.textContent = 'x'
+  tasks.appendChild(btn)
+
+  btn.addEventListener('click', deleteTasks)
+}
+
+//function to remove the tasks by clicking the 'x' button
+function deleteTasks(e) {
+  e.target.parentNode.remove()
+}
